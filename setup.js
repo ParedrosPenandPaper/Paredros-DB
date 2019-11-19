4ff3734eefe5f4f59c3f167daaea78edbf508c95
Ismael Agchar <agcharis67705@th-nuernberg.de>
2019-11-19 20:27:51 +0100

    updated setup script

--- setup.js
+++ setup.js
@@ -1,4 +1,12 @@
-const db = connect('localhost:27017/paredros')
-db.createCollection('adventures')
-db.createCollection('dataElements')
-db.createCollection('users')
+var conn = new Mongo("localhost:27017");
+var db = conn.getDB("paredros");
+
+db.createCollection("adventures");
+db.createCollection("dataElements");
+db.createCollection("users");
+
+var testObj = { foo: "bar", test: true };
+
+db.adventures.insertOne(testObj);
+db.dataElements.insertOne(testObj);
+db.users.insertOne(testObj);
